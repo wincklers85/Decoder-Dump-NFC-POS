bf6d690d853153996d7d19b7ef0aa5394d4d2f44
wincklers85 <wincklers85@googlemail.com>
2026-09-17 16:25:40 +0200

    Add offline service worker

---
+++ docs/sw.js
@@ -0,0 +1,5 @@
+const CACHE='nfc-pos-decoder-v1';
+const FILES=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
